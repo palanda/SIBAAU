7dbd2c01a396ebefa59098ca0a31a1aabc0a73ec
yorigum <yorigum@gmail.com>
2019-03-31 00:23:11 +0700

    init commit

---
+++ WpfApp1/MainMenu.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BengkelAtmaAuto
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class mainMenu : Window
+    {
+        public mainMenu()
+        {
+            InitializeComponent();
+        }
+
+
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+           
+            if (tbUsername.Text.Equals("panda") && tbPassword.Text.Equals("panda123"))
+            {
+                menu dashboard = new menu();  //code buat pindah menu.,
+                dashboard.Show();
+                this.Close();
+            }
+            else
+            {
+                mainMenu main = new mainMenu();
+                main.Show();
+                this.Close();
+            }
+
+        }
+
+    }
+}
---
+++ WpfApp1/Menu Pegawai.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for Window3.xaml
+    /// </summary>
+    public partial class Window3 : Window
+    {
+        public Window3()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnPegawaiCabang_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
